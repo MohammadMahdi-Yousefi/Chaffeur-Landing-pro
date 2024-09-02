2507cf88e9effb00d79d2ac777fa1779ee406bab
[Mohammad Mahdi Yousefi] <[mohamadmahdiusfee@gmail.com]>
2024-09-02 15:00:37 +0330

    finished testimonials

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,36 @@ BtnNavEl.addEventListener("click", function () {
   HeaderEl.classList.toggle("nav-open");
 });
 
+
+// fleet-section slider function
+const carSlider = new Swiper(".car-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination-custom",
+    clickable: false,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".prev-btn",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 29,
+    },
+  },
+});
+
+// testimonials-section slider function
+// const testSLider = new Swiper(".test-slider", {
+//   spaceBetween: 0,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+  
+  
+// });
+
+
